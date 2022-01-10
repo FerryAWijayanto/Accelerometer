12143e1ced82a005b3f5ac61da0382c52ad9fde7
FerryAWijayanto <ferry.adiwijayanto@outlook.com>
2022-01-10 11:01:55 +0700

    Initial commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Ferry Adi Wijayanto on 10/01/22.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels(data:error:))
+    }
+
+
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "X: \(x)"
+        yLabel.text = "Y: \(y)"
+        zLabel.text = "Z: \(z)"
+    }
+}
+
